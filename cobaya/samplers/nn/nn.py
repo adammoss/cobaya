063b2ce877e9731853db0af29d428b468be6eeb9
Adam Moss <adam.moss@nottingham.ac.uk>
2019-12-02 23:02:45 +0000

    NNest

--- cobaya/samplers/nn/nn.py
+++ cobaya/samplers/nn/nn.py
@@ -15,7 +15,7 @@ import logging
 from itertools import chain
 
 # Local
-from cobaya.tools import read_dnumber, get_external_function, relative_to_int
+from cobaya.tools import read_dnumber, get_external_function
 from cobaya.sampler import Sampler
 from cobaya.mpi import get_mpi_comm
 from cobaya.mpi import am_single_or_primary_process, more_than_one_process, sync_processes
@@ -26,7 +26,7 @@ from cobaya.yaml import yaml_dump_file
 
 class nn(Sampler):
     def initialize(self):
-        """Imports the NNest sampler and prepares its arguments."""
+        """Imports the nnest sampler and prepares its arguments."""
         if am_single_or_primary_process():  # rank = 0 (MPI master) or None (no MPI)
             self.log.info("Initializing")
         # If path not given, try using general path to modules
@@ -34,17 +34,12 @@ class nn(Sampler):
             self.path = get_path(self.path_install)
         if self.path:
             if am_single_or_primary_process():
-                self.log.info("Importing *local* NNest from " + self.path)
+                self.log.info("Importing *local* nnest from " + self.path)
                 if not os.path.exists(os.path.realpath(self.path)):
                     raise LoggedError(self.log, "The given path does not exist. "
                                                 "Try installing NNest with "
                                                 "'cobaya-install nnest -m [modules_path]")
-            pc_build_path = get_build_path(self.path)
-            if not pc_build_path:
-                raise LoggedError(self.log, "Either NNest is not in the given folder, "
-                                            "'%s', or you have not compiled it.", self.path)
-            # Inserting the previously found path into the list of import folders
-            sys.path.insert(0, pc_build_path)
+            sys.path.insert(0, self.path)
         else:
             self.log.info("Importing *global* NNest.")
         try:
@@ -52,7 +47,7 @@ class nn(Sampler):
             self.nnest = nnest
         except ImportError:
             raise LoggedError(
-                self.log, "Couldn't find the NNest python interface. "
+                self.log, "Couldn't find the nnest python interface. "
                           "Make sure that you have compiled it, and that you either\n"
                           " (a) specify a path (you didn't) or\n"
                           " (b) install the Python interface globally with\n"
@@ -63,9 +58,7 @@ class nn(Sampler):
                          len(self.model.prior) + len(self.model.likelihood._likelihoods))
         if self.logzero is None:
             self.logzero = np.nan_to_num(-np.inf)
-        if self.max_ndead == np.inf:
-            self.max_ndead = -1
-        for p in ["nlive", "nprior", "max_ndead"]:
+        for p in ["nlive", "nprior"]:
             setattr(self, p, read_dnumber(getattr(self, p), self.nDims, dtype=int))
         # Fill the automatic ones
         if getattr(self, "feedback", None) is None:
@@ -102,69 +95,45 @@ class nn(Sampler):
         else:
             speeds, blocks = self.model.likelihood._speeds_of_params(int_speeds=True)
         blocks_flat = list(chain(*blocks))
-        self.ordering = [
-            blocks_flat.index(p) for p in self.model.parameterization.sampled_params()]
+        self.ordering = [blocks_flat.index(p) for p in self.model.parameterization.sampled_params()]
         self.grade_dims = np.array([len(block) for block in blocks])
-        # bugfix: pypolychord's C interface for Fortran does not like int numpy types
         self.grade_dims = [int(x) for x in self.grade_dims]
-        # Steps per block
-        # NB: num_repeats is ignored by PolyChord when int "grade_frac" given,
-        # so needs to be applied by hand.
-        # Make sure that speeds are integer, and that the slowest is 1,
-        # for a straightforward application of num_repeats
-        speeds = relative_to_int(speeds, 1)
-        # In num_repeats, `d` is interpreted as dimension of each block
-        self.grade_frac = [
-            int(speed * read_dnumber(self.num_repeats, dim_block))
-            for speed, dim_block in zip(speeds, self.grade_dims)]
         # prior conversion from the hypercube
-        bounds = self.model.prior.bounds(
-            confidence_for_unbounded=self.confidence_for_unbounded)
+        bounds = self.model.prior.bounds(confidence_for_unbounded=self.confidence_for_unbounded)
         # Check if priors are bounded (nan's to inf)
         inf = np.where(np.isinf(bounds))
         if len(inf[0]):
             params_names = self.model.parameterization.sampled_params()
             params = [params_names[i] for i in sorted(list(set(inf[0])))]
             raise LoggedError(
-                self.log, "PolyChord needs bounded priors, but the parameter(s) '"
+                self.log, "nnest needs bounded priors, but the parameter(s) '"
                           "', '".join(params) + "' is(are) unbounded.")
         locs = bounds[:, 0]
         scales = bounds[:, 1] - bounds[:, 0]
         # This function re-scales the parameters AND puts them in the right order
-        self.nn_prior = lambda x: (locs + (np.array(x)[self.ordering] / 2 + 0.5) * scales).tolist()
-        # We will need the volume of the prior domain, since PolyChord divides by it
+        self.transform = lambda x: locs + (np.array(x)[:, self.ordering] / 2 + 0.5) * scales
+        # Volume of the prior domain
         self.logvolume = np.log(np.prod(scales))
-        # Prepare callback function
-        if self.callback_function is not None:
-            self.callback_function_callable = (
-                get_external_function(self.callback_function))
         self.last_point_callback = 0
         self.n_sampled = len(self.model.parameterization.sampled_params())
         self.n_derived = len(self.model.parameterization.derived_params())
         self.n_priors = len(self.model.prior)
         self.n_likes = len(self.model.likelihood._likelihoods)
-        # Done!
 
     def run(self):
-        """
-        Prepares the posterior function and calls ``PolyChord``'s ``run`` function.
-        """
 
-        # Prepare the posterior
-        # Don't forget to multiply by the volume of the physical hypercube,
-        # since PolyChord divides by it
         def logpost(params_values):
-            logposterior, logpriors, loglikes, derived = (
-                self.model.logposterior(params_values))
-            if len(derived) != len(self.model.parameterization.derived_params()):
-                derived = np.full(
-                    len(self.model.parameterization.derived_params()), np.nan)
-            if len(loglikes) != len(self.model.likelihood._likelihoods):
-                loglikes = np.full(
-                    len(self.model.likelihood._likelihoods), np.nan)
-            derived = list(derived) + list(logpriors) + list(loglikes)
-            return (
-                max(logposterior + self.logvolume, 0.99 * self.pc_settings.logzero), derived)
+            logl, der = [], []
+            for p in params_values:
+                logposterior, logpriors, loglikes, derived = (self.model.logposterior(p))
+                if len(derived) != len(self.model.parameterization.derived_params()):
+                    derived = np.full(len(self.model.parameterization.derived_params()), np.nan)
+                if len(loglikes) != len(self.model.likelihood._likelihoods):
+                    loglikes = np.full(len(self.model.likelihood._likelihoods), np.nan)
+                derived = list(derived) + list(logpriors) + list(loglikes)
+                logl.append(max(logposterior, 0.99 * self.logzero))
+                der.append(derived)
+            return np.array(logl), np.array(der)
 
         sync_processes()
         if am_single_or_primary_process():
@@ -172,10 +141,11 @@ class nn(Sampler):
 
         nn = self.nnest.NestedSampler(self.nDims,
                                       logpost,
-                                      transform=self.nn_prior,
-                                      append_run_num=False,
+                                      transform=self.transform,
+                                      append_run_num=True,
                                       log_dir=self.base_dir,
-                                      num_live_points=self.nlive)
+                                      num_live_points=self.nlive,
+                                      num_derived=self.nDerived)
 
         nn.run(dlogz=self.precision_criterion,
                mcmc_batch_size=1)
@@ -183,7 +153,7 @@ class nn(Sampler):
 
 # Installation routines ##################################################################
 
-# Name of the PolyChord repo and version to download
+# Name of the nnest repo and version to download
 pc_repo_name = "adammoss/nnest"
 pc_repo_version = "0.4"
 
@@ -207,14 +177,14 @@ def install(path=None, force=False, code=False, data=False, no_progress_bars=Fal
     if not code:
         return True
     log = logging.getLogger(__name__.split(".")[-1])
-    log.info("Downloading NNest...")
+    log.info("Downloading nnest...")
     success = download_github_release(os.path.join(path, "code"), pc_repo_name,
                                       pc_repo_version, no_progress_bars=no_progress_bars,
                                       logger=log)
     if not success:
-        log.error("Could not download NNest.")
+        log.error("Could not download nnest.")
         return False
-    log.info("Compiling (Py)PolyChord...")
+    log.info("Compiling nnest..")
     from subprocess import Popen, PIPE
     # Needs to re-define os' PWD,
     # because MakeFile calls it and is not affected by the cwd of Popen
